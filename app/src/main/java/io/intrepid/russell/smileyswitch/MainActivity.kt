c095549dc1f50de1f48cbd30c16a272b4e610a39
Russell Wolf <rwolf@intrepid.io>
2016-04-01 17:28:55 -0400

    Animate color change for thumb background

--- app/src/main/java/io/intrepid/russell/smileyswitch/MainActivity.kt
+++ app/src/main/java/io/intrepid/russell/smileyswitch/MainActivity.kt
@@ -1,11 +1,14 @@
 package io.intrepid.russell.smileyswitch
 
+import android.graphics.drawable.LayerDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.view.ViewGroup
 import org.jetbrains.anko.appcompat.v7.switchCompat
 import org.jetbrains.anko.gravity
+import org.jetbrains.anko.onCheckedChange
 import org.jetbrains.anko.verticalLayout
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,15 @@ class MainActivity : AppCompatActivity() {
             gravity = Gravity.CENTER
             switchCompat {
                 setThumbResource(R.drawable.thumb)
+                onCheckedChange { compoundButton, b ->
+                    val thumbBg = ((thumbDrawable as LayerDrawable).getDrawable(0)) as TransitionDrawable
+                    val duration = resources.getInteger(R.integer.animation_duration_ms)
+                    if (b) {
+                        thumbBg.startTransition(duration)
+                    } else {
+                        thumbBg.reverseTransition(duration)
+                    }
+                }
             }.lparams {
                 width = ViewGroup.LayoutParams.WRAP_CONTENT
                 height = ViewGroup.LayoutParams.WRAP_CONTENT
