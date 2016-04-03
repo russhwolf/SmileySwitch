5cadfb6a784c745663e50a6e87d2786d00a6b7c1
Russell <russhwolf@gmail.com>
2016-04-02 22:53:56 -0400

    Head turn based on switch track position, using modified switch class

--- app/src/main/java/io/intrepid/russell/smileyswitch/MainActivity.kt
+++ app/src/main/java/io/intrepid/russell/smileyswitch/MainActivity.kt
@@ -6,7 +6,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.view.ViewGroup
-import org.jetbrains.anko.appcompat.v7.switchCompat
+import android.view.ViewManager
+import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.gravity
 import org.jetbrains.anko.onCheckedChange
 import org.jetbrains.anko.verticalLayout
@@ -18,15 +19,34 @@ class MainActivity : AppCompatActivity() {
 
         verticalLayout {
             gravity = Gravity.CENTER
-            switchCompat {
+            trackableSwitch {
                 setThumbResource(R.drawable.thumb)
-                onCheckedChange { compoundButton, b ->
+                setTrackResource(R.drawable.track)
+                onCheckedChange { compoundButton, isChecked ->
                     val thumbBg = ((thumbDrawable as LayerDrawable).getDrawable(0)) as TransitionDrawable
+                    val track = (trackDrawable) as TransitionDrawable
                     val duration = resources.getInteger(R.integer.animation_duration_ms)
-                    if (b) {
+                    if (isChecked) {
                         thumbBg.startTransition(duration)
+                        track.startTransition(duration)
                     } else {
                         thumbBg.reverseTransition(duration)
+                        track.reverseTransition(duration)
+                    }
+                }
+                setThumbPositionListener { view, position, wasChecked ->
+                    val thumbLayers = thumbDrawable as LayerDrawable
+                    val progress = if (wasChecked) 1 - position else position
+                    val threshold = 0.1f
+                    val offset = if (progress < threshold) progress / threshold else if (progress > 1 - threshold) (1 - progress) / threshold else 1f
+                    val range = resources.getDimension(R.dimen.thumb_head_turn_offset)
+                    val scaledOffset = (offset * range).toInt()
+                    if (wasChecked) {
+                        thumbLayers.setLayerInset(1, -scaledOffset, 0, scaledOffset, 0)
+                        thumbLayers.setLayerInset(2, -scaledOffset, 0, scaledOffset, 0)
+                    } else {
+                        thumbLayers.setLayerInset(1, scaledOffset, 0, -scaledOffset, 0)
+                        thumbLayers.setLayerInset(2, scaledOffset, 0, -scaledOffset, 0)
                     }
                 }
             }.lparams {
@@ -36,3 +56,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+inline fun ViewManager.trackableSwitch() = trackableSwitch {}
+inline fun ViewManager.trackableSwitch(init: TrackableSwitch.() -> Unit) = ankoView({ TrackableSwitch(it) }, init)
---
+++ app/src/main/java/io/intrepid/russell/smileyswitch/TrackableSwitch.kt
@@ -0,0 +1,55 @@
+package io.intrepid.russell.smileyswitch
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import io.intrepid.russell.smileyswitch.io.intrepid.russell.smileyswitch.support.SwitchCompat
+
+/**
+ * This is an extension of the SwitchCompat widget which exposes a listener to track the thumb position.
+ */
+class TrackableSwitch : SwitchCompat {
+    private var thumbPositionListener: ThumbPositionListener? = null
+
+    /**
+     * If the last integer thumb position was 0, this is false.
+     * If it was 1, this is true
+     */
+    private var wasChecked = false
+
+    constructor(context: Context) : super(context) {
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    }
+
+    // This override is impossible using an unmodified version of SwitchCompat
+    override fun setThumbPosition(position: Float) {
+        super.setThumbPosition(position)
+        if (position <= 0) {
+            wasChecked = false
+        }
+        if (position >= 1) {
+            wasChecked = true
+        }
+        thumbPositionListener?.onThumbPositionChanged(this, position, wasChecked)
+    }
+
+    fun setThumbPositionListener(listener: ThumbPositionListener) {
+        thumbPositionListener = listener
+    }
+
+    fun setThumbPositionListener(listener: (View, Float, Boolean) -> Unit) {
+        thumbPositionListener = object : ThumbPositionListener {
+            override fun onThumbPositionChanged(view: TrackableSwitch, position: Float, wasChecked: Boolean)
+                    = listener.invoke(view, position, wasChecked)
+        }
+    }
+
+    interface ThumbPositionListener {
+        fun onThumbPositionChanged(view: TrackableSwitch, position: Float, wasChecked: Boolean)
+    }
+}
